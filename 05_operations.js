c8cc42f3924fba919cb9154c769a030b9382d9f0
Souvik Chatterjee <souvikchatterjee@Souviks-MacBook-Air.local>
2025-06-22 08:14:46 +0530

    Added 05_operations.js file

---
+++ 05_operations.js
@@ -0,0 +1,16 @@
+let value = 3
+
+console.log(value );
+
+a = 12
+b = ++a
+
+console.table([a,b])
+
+x = 13
+y = x++
+
+console.table([x,y])
+
+//prefix me a ke value me 1 bara and a and b dono me hi wo bada value assign huwa. 
+// and postfix me b x ke value me 1 bara but y ke value x ke previous value ka equal huwa.
